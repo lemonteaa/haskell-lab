f4456e64c8c490c9fa3374cf122b5e6670cf4f24
lemonteaa <lemontea.Tom@gmail.com>
2016-06-08 23:49:16 +0800

    Change all

--- learning/Charger.hs
+++ learning/Charger.hs
@@ -1,8 +1,19 @@
 {-# LANGUAGE RecordWildCards #-}
+import Data.List
+
 data Currency = Currency { unit :: Rational, count :: Integer }
 type Reserve = [Currency]
 
 changeOne :: Rational -> Currency -> (Rational, Integer)
 changeOne amount Currency{..} =
-	let changeCount = min count $ toInteger . floor $ amount/unit in
-		(amount - toRational changeCount * unit, changeCount)
+    let changeCount = min count $ toInteger . floor $ amount/unit
+        remainder = amount - toRational changeCount * unit
+    in (remainder, changeCount)
+
+--(Rational, [Currency], Reserve)
+changeAllGreedy :: Rational -> Reserve -> (Rational, [Currency])
+changeAllGreedy amount reserve =
+    foldl' changeOneDetail (amount, []) reserve
+    where changeOneDetail (remainder, changes) c@Currency{ unit = cunit } =
+              let (newRemainder, changeCount) = changeOne remainder c
+              in (newRemainder, Currency{ unit = cunit, count = changeCount} : changes)
